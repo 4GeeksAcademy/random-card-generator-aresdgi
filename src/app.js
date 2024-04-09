6567f0383a5d18ca06237f7937cb7214c77c632e
Aresdgi <115067719+Aresdgi@users.noreply.github.com>
2024-04-09 16:39:26 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomCard() {
+  const suits = ["♥️", "♠️", "♣️", "♦️"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const randomSuit = suits[getRandomInt(0, suits.length - 1)];
+  const randomValue = values[getRandomInt(0, values.length - 1)];
+
+  return {
+    suit: randomSuit,
+    value: randomValue
+  };
+}
+
+function displayCard(card) {
+  const cardElement = document.getElementById("card");
+  cardElement.innerHTML = `
+    <div class="${getSuitClass(card.suit)}">${card.suit}</div>
+    <div class="value">${card.value}</div>
+    <div class="${getSuitClass(card.suit)}">${card.suit}</div>
+  `;
+}
+
+function getSuitClass(suit) {
+  switch (suit) {
+    case "♥️":
+    case "♦️":
+      return "red-suit";
+    case "♠️":
+    case "♣️":
+      return "black-suit";
+    default:
+      return "";
+  }
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomCard = generateRandomCard();
+  displayCard(randomCard);
 };
